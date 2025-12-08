f6fddf31424b9b8b313cfda493db067e0782d384
Craig Dennis <craigsdennis@gmail.com>
2025-12-07 16:47:40 -0800

    Adds description as required.

--- worker/agents/booth.ts
+++ worker/agents/booth.ts
@@ -2,16 +2,19 @@ import { Agent, callable } from "agents";
 
 export type BoothState = {
     displayName: string;
+    description: string;
     backgroundImageUrl: string;
 }
 
 export class BoothAgent extends Agent<Env, BoothState> {
 
-    setup({displayName}: {displayName: string}) {
+    setup({displayName, description}: {displayName: string, description: string}) {
+        // TODO: Generate the background
         // Update the state, auto broadcasts
         this.setState({
             ...this.state,
             displayName,
+            description
         })
     }
 }
